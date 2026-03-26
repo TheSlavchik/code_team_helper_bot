feb935efcb387c38e23f87cf91f18e68d265fd2d
Viacheslav Dev <102011344+TheSlavchik@users.noreply.github.com>
2026-03-26 12:50:56 +0600

    feat: to inline translation

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,5 @@
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
 from aiogram import F, Router
 import app.keyboards as kb 
 
@@ -9,9 +9,30 @@ router = Router()
 async def handle_start(message: Message):
     await message.answer("Добро пожаловать!", reply_markup=kb.main)
 
-@router.message(Command("help"))
+#Projects
+
+@router.callback_query(F.data == "projects_menu")
+async def to_projects_inline(callback: CallbackQuery):
+    await callback.message.edit_text("Выберите действие:", reply_markup=kb.projects)
+    await callback.answer()
+
+#Profile
+
+@router.callback_query(F.data == "user_profile")
+async def to_profile_inline(callback: CallbackQuery):
+    await callback.message.edit_text("Выберите действие:", reply_markup=kb.profile)
+    await callback.answer()
+
+#Help
+
+@router.message(F.text == "❓ Помощь")
 async def get_help(message: Message):
-    await message.answer("Это помощь тебе, друн!")
+    await message.answer("")
+
+@router.callback_query(F.data == "back_to_main")
+async def back_to_main_inline(callback: CallbackQuery):
+    await callback.message.edit_text("Вы вернулись в главное меню", reply_markup=kb.main)
+    await callback.answer()
 
 @router.callback_query(F.data == "sosiska")
 async def get_help(callback: CallbackQuery):
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,18 +1,43 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 
-main = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="👤 Мой профиль"), KeyboardButton(text="🔍 Найти проекты")],
-        [KeyboardButton(text="🛠 Создать проект"), KeyboardButton(text="📈 Рекомендации")],
-        [KeyboardButton(text="❓ Помощь")]
-    ],
-
-    resize_keyboard=True,
-    input_field_placeholder="Выберите пункт меню"
+main = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="👤 Мой профиль", callback_data="user_profile"),
+            InlineKeyboardButton(text="🔍 Проекты", callback_data="projects_menu")
+        ],
+        [
+            InlineKeyboardButton(text="❓ Помощь", callback_data="help_menu")
+        ]
+    ]
 )
 
+
 profile = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="👤 Изменить данные профиля", callback_data="change_profile")],
+        [InlineKeyboardButton(text="Назад", callback_data="back_to_main")]
+    ]
+)
+
+projects = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Мои проекты", callback_data="test"), 
+            InlineKeyboardButton(text="Создать проект", callback_data="test")
+        ],
+        [
+            InlineKeyboardButton(text="Поиск по проектам", callback_data="test"),
+            InlineKeyboardButton(text="Рекомендации", callback_data="test")
+        ],
+        [
+            InlineKeyboardButton(text="Назад", callback_data="back_to_main")
+        ]
+    ]
+)
+
+test = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Изменить данные", callback_data="change_profile")],
         [InlineKeyboardButton(text="Мои проекты", callback_data="my_projects")],
